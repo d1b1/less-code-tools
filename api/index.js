bc1d61bbfee62a26a05d9bbb899508a893116fca
Stephan L. Smith <stephan@meenta.io>
2024-05-01 12:20:16 -0400

    Added endpoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-module.exports = async (req, res) => {
-    res.send("Hello from Render!");
-};
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.send('Hello from Render!');
+});
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
